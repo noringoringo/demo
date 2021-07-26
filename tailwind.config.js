6010aae0252b04d35267312908a480cfa245e1d4
noringoringo <87978293+noringoringo@users.noreply.github.com>
2021-07-26 23:49:00 +0900

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily:{
+      body:[
+        '"Mv Boli"',
+      ]
+    }
+  },
+  variants: {
+    translate:['responsive','hover','focus','motion-safe'],
+    extend: {},
+  },
+  plugins: [],
+}
